86f5730e18df60823283f9c5f6ce5509367f3a62
gayan-gapstars <mgayansameera@gmail.com>
2024-02-17 22:55:15 +0530

    First commit

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ForgotController;
 use App\Http\Controllers\IbanValidityController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
